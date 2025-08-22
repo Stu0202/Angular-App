3f7287db8e54c80f4dc3fad1a4e6f1835229c666
Jorge <jorgereyeslira02@gmail.com>
2025-08-21 20:03:39 -0500

    Separar template de la logica de nuestro componente usando templateUrl

--- src/app/pages/counter/counter-page.component.ts
+++ src/app/pages/counter/counter-page.component.ts
@@ -3,16 +3,26 @@ import { Component } from "@angular/core";
 
 
 @Component({
-  template: `
-  <h1>Counter: {{ counter}}</h1>
-  <button (click)="increaseBy(1)" >+1</button>
+  templateUrl: './counter-page.component.html',
+  styles: `
+    button{
+      padding: 5px;
+      margin: 5px 10px;
+      width:75px
+    }
   `
 })
 export class CounterPageComponent {
 
-  counter = 20;
+  counter = 10;
 
   increaseBy(value: number) {
     this.counter += value;
   }
+
+  resetCounter() {
+    this.counter = 10
+  }
+
 }
+
