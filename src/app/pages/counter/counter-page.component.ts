2c4c5151d77744dc063c7712db55f2fab25699f8
Jorge <jorgereyeslira02@gmail.com>
2025-08-21 21:05:45 -0500

    Señales

--- src/app/pages/counter/counter-page.component.ts
+++ src/app/pages/counter/counter-page.component.ts
@@ -1,5 +1,5 @@
 
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 
 @Component({
@@ -15,13 +15,16 @@ import { Component } from "@angular/core";
 export class CounterPageComponent {
 
   counter = 10;
+  counterSignal = signal(10)
 
   increaseBy(value: number) {
     this.counter += value;
+    this.counterSignal.update((current) => current + value)
   }
 
   resetCounter() {
-    this.counter = 10
+    this.counter = 0
+    this.counterSignal.set(0)
   }
 
 }
