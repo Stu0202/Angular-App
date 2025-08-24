5e052579e4d9bfdf4d680730b2785d2878e23b8a
Jorge <jorgereyeslira02@gmail.com>
2025-08-23 20:28:08 -0500

    Usando RouterLinkActive para agregar estilos de acuerdo a la ruta donde nos encontremos

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
 
 })
