5e3664907d5e02d44e06f2b489ba01ec38e017a4
Jorge <jorgereyeslira02@gmail.com>
2025-08-24 13:46:11 -0500

    Añadiendo eventos a los inputs para agregar un nuevo personaje

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -17,6 +17,9 @@ interface Character {
 
 })
 export class DragonballPageComponent {
+
+  name = signal('Gohan')
+  power = signal(100)
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8000 },
@@ -29,4 +32,24 @@ export class DragonballPageComponent {
   //     'text-danger': true
   //   }
   // })
+
+  addCharacter() {
+    if (!this.name() || !this.power() || this.power() <= 0) {
+      return;
+    }
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    }
+
+    this.characters.update((list) => [...list, newCharacter])
+    this.resetFields()
+  }
+
+  resetFields() {
+    this.name.set('')
+    this.power.set(0)
+  }
 }
+
