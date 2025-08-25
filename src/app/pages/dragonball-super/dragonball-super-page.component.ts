d3e52c3348bb1b502d4bd1911545bad0f898ee2f
Jorge <jorgereyeslira02@gmail.com>
2025-08-25 13:05:26 -0500

    Singal Inputs y creacion de componentes para mejorar la logica

--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,6 +1,7 @@
 
 import { Component, signal } from "@angular/core";
 import { CharacterListComponent } from "../../components/dragonball/character-list/character-list.component";
+import { CharacterAddComponent } from "../../components/dragonball/character-add/character-add.component";
 
 
 interface Character {
@@ -13,7 +14,7 @@ interface Character {
 @Component({
   templateUrl: './dragonball-super-page.component.html',
   selector: 'dragonball-super',
-  imports: [CharacterListComponent]
+  imports: [CharacterListComponent, CharacterAddComponent]
 
 })
 export class DragonballSuperPageComponent {
@@ -33,12 +34,14 @@ export class DragonballSuperPageComponent {
       return;
     }
     const newCharacter: Character = {
-      id: this.characters().length + 1,
+      //id: this.characters().length + 1,
+      id: 1000,
       name: this.name(),
       power: this.power()
     }
 
-    this.characters.update((list) => [...list, newCharacter])
+    //this.characters.update((list) => [...list, newCharacter])
+    console.log({ newCharacter })
     this.resetFields()
   }
 
